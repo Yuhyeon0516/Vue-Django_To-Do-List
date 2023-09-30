23a710b8859823f7790a05b57b40727810ff9a2f
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-30 23:57:11 +0900

    Created to do delete function in django.

--- combine_vue_and_django/api/views.py
+++ combine_vue_and_django/api/views.py
@@ -1,15 +1,27 @@
-from django import http
 from django.http import HttpResponse, JsonResponse
-from django.views.generic import ListView
+from django.views.generic.edit import BaseDeleteView
+from django.views.generic.list import BaseListView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 from todo.models import Todo
 
 
 # Create your views here.
-class ApiTodoLV(ListView):
+class ApiTodoLV(BaseListView):
     model = Todo
 
     def render_to_response(self, context, **response_kwargs):
         todoList = list(context["object_list"].values())
 
         return JsonResponse(data=todoList, safe=False)
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class ApiTodoDelV(BaseDeleteView):
+    model = Todo
+
+    def delete(self, request, *args, **kwarg):
+        self.object = self.get_object()
+        self.object.delete()
+        return JsonResponse(data={}, status=204)
