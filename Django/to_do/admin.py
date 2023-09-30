cb7c0d19decf3b76b08d6ab83bc548deb314c58f
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-30 14:29:36 +0900

    Created to do model & show to do in admin page.

--- Django/to_do/admin.py
+++ Django/to_do/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from to_do.models import Todo
+
+
 # Register your models here.
+@admin.register(Todo)
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "todo")
