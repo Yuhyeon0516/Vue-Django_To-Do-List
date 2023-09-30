968c8753aae9d87817531b716deb758e1b9847de
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-30 23:23:34 +0900

    Created to do list get method in django.

--- combine_vue_and_django/api/views.py
+++ combine_vue_and_django/api/views.py
@@ -1,3 +1,4 @@
+from django import http
 from django.http import HttpResponse, JsonResponse
 from django.views.generic import ListView
 
@@ -8,12 +9,7 @@ from todo.models import Todo
 class ApiTodoLV(ListView):
     model = Todo
 
-    def get(self, request, *args, **kwargs):
-        tmpList = [
-            {"id": 1, "name": "d김유현", "todo": "Django 와 Vue.js 연동 프로그램을 만들고 있습니다."},
-            {"id": 2, "name": "d홍길동", "todo": "이름을 안쓰면 홍길동으로 나와요..."},
-            {"id": 3, "name": "d이순신", "todo": "신에게는 아직 열두 척의 배가 있사옵니다."},
-            {"id": 4, "name": "d성춘향", "todo": "그네 타기"},
-        ]
+    def render_to_response(self, context, **response_kwargs):
+        todoList = list(context["object_list"].values())
 
-        return JsonResponse(data=tmpList, safe=False)
+        return JsonResponse(data=todoList, safe=False)
